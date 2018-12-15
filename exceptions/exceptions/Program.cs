4bce9f13fa6e0d97b7a34ddcec254e544c8b5bd8
DeliaOros <delia_iletchi@yahoo.com>
2018-12-15 12:29:27 +0200

    lucrat in clasa

--- exceptions/exceptions/Program.cs
+++ exceptions/exceptions/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 
-namespace exceptions
+namespace exceptionsProject
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //    try
+            //    {
+            //        int b = 100;
+            //        int a = b / 0;
+            //        Console.WriteLine(a);
+
+            //    }
+            //    catch (DivideByZeroException exception)
+            //    {
+            //        Console.WriteLine("Ooops!Something happened.");
+            //        Console.WriteLine(exception.Message);
+            //        Console.WriteLine(exception.StackTrace);
+            //    }
+            try
+            {
+                Person person = new Person(15);
+                Person person1 = new Person(-1);
+                Person person2 = new Person(2);
+                Person person3 = new Person(81);
+            }
+
+            catch (ArgumentException e)
+            {
+
+
+                //this will show an error
+                //cw        e
+                Console.WriteLine(e.Message + e.GetType());
+
+                //throw;
+            }
+            catch (DressCodeException ex)
+            {
+                Console.WriteLine(ex.Message + ex.GetType());
+
+            }
+
+
         }
+
+
     }
 }
+
