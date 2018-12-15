c573df2d3ce31d1f6bd9d03743d81d77559a4c31
DeliaOros <delia_iletchi@yahoo.com>
2018-12-15 13:38:02 +0200

    added Collections project

---
+++ exceptions/Collections/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myStringList = new List<string>();
+            myStringList.Add("C#");
+            myStringList.Add("Java");
+            myStringList.Add("Python");
+            myStringList.Add("C++");
+            foreach (var item in myStringList)
+            {
+                Console.WriteLine(item);
+            }
+            if (myStringList.Contains("Java"))
+            {
+                myStringList.Remove("Java");
+            }
+            Zoo myAwsomeZoo = new Zoo();
+
+            foreach(var animal in myAwsomeZoo.Animals)
+            {
+                Console.WriteLine($"This is a {animal.Name} and its a {animal.Gender}");
+            }
+        }
+    }
+}
